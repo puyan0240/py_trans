4b91fcd63adf88425b2242bb0a6690f0abff0986
puyan0240-2 <matusima0604@zeus.eonet.ne.jp>
2022-08-21 15:56:18 +0900

    テストコード

---
+++ test.py
@@ -0,0 +1,17 @@
+from socket import TCP_NODELAY
+from googletrans import Translator
+
+
+trans = Translator()
+
+try:
+    bf = "今日は暑いですね"
+    af = trans.translate(bf, "en")
+    print(bf+" -> "+af.text)
+
+    bf = "it is hot today"
+    af = trans.translate(bf, "ja")
+    print(bf+" -> "+af.text)
+
+except Exception as e:
+    print(e)
