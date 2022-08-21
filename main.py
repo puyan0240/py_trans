1177f485b5c2a9f5c8495df3c97fa8f2c58e08a1
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-21 19:27:26 +0900

    ボタン仮実装

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ root = tkinter.Tk()
 root.title("Trans")
 root.geometry("600x400")
 
+frame_top = tkinter.Frame(root)
+frame_top.propagate(0)
+frame_top.pack(side=tkinter.TOP)
+
+btn_trans = tkinter.Button(frame_top, text="翻訳", width=15)
+btn_trans.grid(row=0, column=0, sticky=tkinter.W)
+
+btn_clr = tkinter.Button(frame_top, text="取消", width=15)
+btn_clr.grid(row=0, column=1, sticky=tkinter.E)
+
+
+
 #root.resizable(False, False)    #ウィンドウサイズ固定
 root.mainloop()
