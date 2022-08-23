1177aebd9d77ad1c8d6330a3b95424992d1f62dc
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-24 00:22:03 +0900

    翻訳ボタン押下後の翻訳処理の仮実装

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import ttk
+import googletrans
+from googletrans import Translator
 
 
 #言語テーブル
@@ -18,22 +20,62 @@ lang_tbl = [
 ]
 
 
+############################################################
+#翻訳ボタンが押されました
+############################################################
+def btn_trans_clicked():
+
+    #-------------------------------------------------------
+    #翻訳元（入力）の文字取得
+    #-------------------------------------------------------
+    input_text = text_src.get('1.0', 'end')
+    #print(input_text)
+
+    lang_src = lang_dst = ""
+    #-------------------------------------------------------
+    #翻訳言語取得
+    #-------------------------------------------------------
+    #翻訳元
+    if cb_src.get() != "自動":
+        for lang_tbl_line in lang_tbl:
+            if lang_tbl_line[LANG_TBL_NAME] == cb_src.get():
+                lang_src = lang_tbl_line[LANG_TBL_PARAME]
+                break
+    #翻訳先
+    for lang_tbl_line in lang_tbl:
+        if lang_tbl_line[LANG_TBL_NAME] == cb_dst.get():
+            lang_dst = lang_tbl_line[LANG_TBL_PARAME]
+            break
+
+    #print(lang_src)
+    #print(lang_dst)
+
+
+    #-------------------------------------------------------
+    #翻訳実行
+    #-------------------------------------------------------
+    trans = Translator()
+    try:
+        if lang_src == "":  #翻訳元=自動
+            result = trans.translate(input_text, dest=lang_dst)
+        else:   #翻訳元=指定あり
+            result = trans.translate(input_text, src=lang_src, dest=lang_dst)
+        print(result.text)
+    except Exception as e:
+        print(e)
+    return
+
+
+
+
 root = tkinter.Tk()
 root.title("Trans")
 root.geometry("1000x600")
 
 frame_top = tkinter.Frame(root)
-#frame_top.propagate(0)
 frame_top.pack()
 
-btn_trans = tkinter.Button(frame_top, text="翻訳", width=15)
-btn_trans.grid(row=0, column=0, sticky=tkinter.W)
-
-#btn_clr = tkinter.Button(frame_top, text="取消", width=15)
-#btn_clr.grid(row=0, column=1, sticky=tkinter.E)
-
 frame_trans = tkinter.Frame(root)
-#frame_trans.propagate(0)
 frame_trans.pack()
 
 #-----------------------------------------------------------
@@ -58,6 +100,11 @@ text_src = tkinter.Text(frame_trans, width=40)
 text_src.grid(row=2, column=0)
 
 
+#-----------------------------------------------------------
+#翻訳ボタン
+#-----------------------------------------------------------
+btn_trans = tkinter.Button(frame_trans, text="翻訳", width=15, command=btn_trans_clicked)
+btn_trans.grid(row=0, column=1, rowspan=3)
 
 
 #-----------------------------------------------------------
@@ -65,7 +112,7 @@ text_src.grid(row=2, column=0)
 #-----------------------------------------------------------
 #ラベル
 label_dst = tkinter.Label(frame_trans, text="翻訳先")
-label_dst.grid(row=0, column=1)
+label_dst.grid(row=0, column=2)
 
 
 #Combobox
@@ -75,11 +122,11 @@ for val in lang_tbl:
 v_dst = tkinter.StringVar()
 cb_dst = ttk.Combobox(frame_trans, textvariable=v_dst, values=cb_dst_menu, state="readonly", width=20)
 cb_dst.current(0)
-cb_dst.grid(row=1, column=1)
+cb_dst.grid(row=1, column=2)
 
 #Text
 text_dst = tkinter.Text(frame_trans, width=40)
-text_dst.grid(row=2, column=1)
+text_dst.grid(row=2, column=2)
 
 
 
